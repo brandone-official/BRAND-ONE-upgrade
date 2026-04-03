7f50b8ee20eddfc727ed198451835f5b484011bd
BRAND ONE <jhndy20170101@gmail.com>
2026-04-03 21:43:26 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Service from "./pages/Service";
+import Portfolio from "./pages/Portfolio";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <Router basename="/BRAND-ONE-upgrade"> 
-      <div className="min-h-screen bg-white">
+    <Router>
+      <ScrollToTop />
+      <div className="min-h-screen bg-white flex flex-col">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/portfolio" element={<div className="min-h-screen flex items-center justify-center pt-24 text-2xl font-bold">Portfolio Page (Coming Soon)</div>} />
-        </Routes>
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
